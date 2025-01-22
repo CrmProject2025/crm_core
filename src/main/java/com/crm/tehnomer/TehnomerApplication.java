ae4cbc5ccd8ffbcaf426bdc9ee1952be809edfe0
Tework123 <daniliesaec@gmail.com>
2025-01-22 05:57:56 +0300

    init project

---
+++ src/main/java/com/crm/tehnomer/TehnomerApplication.java
@@ -0,0 +1,13 @@
+package com.crm.tehnomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TehnomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TehnomerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crm/tehnomer/TehnomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crm.tehnomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TehnomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
